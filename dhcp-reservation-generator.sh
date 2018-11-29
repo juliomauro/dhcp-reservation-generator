3d56030dc970b0c5666b78b1332990c1bc1f9bb4
Julio Cesar Mauro <juliomauro@gmail.com>
2018-11-29 17:40:58 -0200

    Update dhcp-reservation-generator.sh

--- dhcp-reservation-generator.sh
+++ dhcp-reservation-generator.sh
@@ -3,9 +3,9 @@ INPUT=ips.csv
 OLDIFS=$IFS
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read macaddress ipaddress user
+while read macaddress ipaddress hostname
 do
-     echo "host $user {"
+     echo "host $hostname {"
      echo "next-server $user;"
      echo "hardware ethernet $macaddress;"
      echo "fixed-address $ipaddress;"
